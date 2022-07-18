9cc4e9ea05f2be91cc02fb1f832e1baa14a41591
lhh1171 <2807652103@qq.com>
2022-07-18 17:55:02 +0800

    second

--- src/main/java/input/SQLAnalyzer/EX.java
+++ src/main/java/input/SQLAnalyzer/EX.java
@@ -3,116 +3,290 @@ package input.SQLAnalyzer;
 import input.ActionEntity;
 import input.RequestEntity;
 
-import java.util.HashMap;
-
 public class EX {
-    public static int state = 0; // 记录DFA的状态
-    public final static HashMap<String,Integer> PostState=new HashMap<String,Integer>();
-    public static void DFA(ActionEntity actionEntity){
-        RequestEntity requestEntity;
+    public static int MethodState = 0; // method
+    public static int PutState=0;
+    public static int DeleteState=0;
+    public static int PostState=0;
+    public static RequestEntity requestEntity;
+//    public static void DFA(ActionEntity actionEntity){
+//        if (isPut(actionEntity)) {
+//            MethodState = 1;
+//        } else if (isDelete(actionEntity)) {
+//            MethodState = 2;
+//        } else if (isPost(actionEntity)) {
+//            MethodState = 3;
+//        } else {
+//            MethodGetErr(actionEntity);
+//        }
+//        switch (MethodState){
+//            case 1:
+//                if (isCreateDB(actionEntity)) {
+//                    PutState=1;
+//                    requestEntity=getCreateDB();
+//                    createDB(requestEntity);
+//                    break;
+//                } else if (isCreateTable(actionEntity)) {
+//                    PutState=2;
+//                    requestEntity=getCreateTable();
+//                    createTable(requestEntity);
+//                    break;
+//                } else if (isPutCells(actionEntity)) {
+//                    PutState=3;
+//                    requestEntity=getPutCells();
+//                    putCells(requestEntity);
+//                    break;
+//                } else {
+//                    putErr(actionEntity);
+//                    break;
+//                }
+//            case 2:
+//                if (isDeleteDB(actionEntity)) {
+//                    DeleteState=1;
+//                    requestEntity=getDeleteDB();
+//                    deleteDB(requestEntity);
+//                    break;
+//                } else if (isDeleteTable(actionEntity)) {
+//                    DeleteState=2;
+//                    requestEntity=getDeleteTable();
+//                    deleteTable(requestEntity);
+//                    break;
+//                } else {
+//                    deleteErr(actionEntity);
+//                    break;
+//                }
+//                break;
+//            case 3:
+//                if(isAlterTable()){
+//                    requestEntity=getAlterTable(actionEntity);
+//                    alterTable(requestEntity);
+//                    break;
+//                }else if(isOpenTransaction()){
+//                    requestEntity=getOpenTransaction(actionEntity);
+//                    openTransaction(requestEntity);
+//                    break;
+//                }else if(isCloseTransaction()){
+//                    requestEntity=getCloseTransaction(actionEntity);
+//                    closeTransaction(requestEntity);
+//                }else if(isMergeVersion()){
+//                    requestEntity=getMergeVersion(actionEntity);
+//                    mergeVersion(requestEntity);
+//                    break;
+//                }else if(isUseVersion()){
+//                    requestEntity=getUseVersion(actionEntity);
+//                    useVersion(requestEntity);
+//                    break;
+//                }else if(isShowVersion()){
+//                    requestEntity=getShowVersion(actionEntity);
+//                    showVersion(requestEntity);
+//                    break;
+//                }else if(isSingleSearch()){
+//                    requestEntity=getSingleSearch(actionEntity);
+//                    singleSearch(requestEntity);
+//                    break;
+//                }else if(isDeleteCells()){
+//                    requestEntity=getDeleteCells(actionEntity);
+//                    deleteCells(requestEntity);
+//                    break;
+//                }else if(isUpdateCells()){
+//                    requestEntity=getUpdateCells(actionEntity);
+//                    updateCells(requestEntity);
+//                    break;
+//                }else if(isMultiSearch()){
+//                    requestEntity=getMultiSearch(actionEntity);
+//                    multiSearch(requestEntity);
+//                    break;
+//                }else if(isDeleteVersion()){
+//                    requestEntity=getDeleteVersion(actionEntity);
+//                    deleteVersion(requestEntity);
+//                    break;
+//                }
+//                break;
+//            default:
+//                System.out.println("未知错误---------------------");
+//        }
+//    }
+    public static void DFA2(ActionEntity actionEntity){
         if (isPut(actionEntity)) {
-            state = 1;
+            MethodState = 1;
         } else if (isDelete(actionEntity)) {
-            state = 2;
+            MethodState = 2;
         } else if (isPost(actionEntity)) {
-            state = 3;
+            MethodState = 3;
         } else {
             MethodGetErr(actionEntity);
         }
-        switch (state){
+        switch (MethodState){
             case 1:
-                if (isCreateDB(actionEntity)) {
-                    requestEntity=getCreateDB();
-                    createDB(requestEntity);
-                    break;
-                } else if (isCreateTable(actionEntity)) {
-                    requestEntity=getCreateTable();
-                    createTable(requestEntity);
-                    break;
-                } else if (isPutCells(actionEntity)) {
-                    requestEntity=getPutCells();
-                    putCells(requestEntity);
-                    break;
-                } else {
-                    putErr(actionEntity);
-                    break;
-                }
+                handlePut(actionEntity);
             case 2:
-                if (isDeleteDB(actionEntity)) {
-                    requestEntity=getDeleteDB();
-                    deleteDB(requestEntity);
-                    break;
-                } else if (isDeleteTable(actionEntity)) {
-                    requestEntity=getDeleteTable();
-                    deleteTable(requestEntity);
-                    break;
-                }  else {
-                    deleteErr(actionEntity);
-                    break;
-                }
-                break;
+                handleDelete(actionEntity);
             case 3:
                 handlePost(actionEntity);
-                break;
             default:
                 System.out.println("未知错误---------------------");
         }
     }
 
-    private static void handlePost(ActionEntity actionEntity) {
-        int PState=PostState.get(actionEntity.getUrl());
-        RequestEntity requestEntity;
-        switch (PState){
-            case 1:
+    private static void MethodGetErr(ActionEntity actionEntity) {
+    }
+
+    private static void handlePut(ActionEntity actionEntity) {
+        String[] putUrl= actionEntity.getMethod().split("/");
+        switch (putUrl.length){
+            case 2:
+                requestEntity=getCreateDB(actionEntity);
+                createDB(requestEntity);
+                break;
+            case 3:
                 requestEntity=getCreateTable(actionEntity);
                 createTable(requestEntity);
                 break;
+            default:
+                System.out.println("the URL Segment is error"+"给出提示（把PUT所有的命令返还给他");
+        }
+    }
+
+    private static RequestEntity getCreateTable(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getCreateDB(ActionEntity actionEntity) {
+    }
+
+
+    private static void handleDelete(ActionEntity actionEntity) {
+        String[] deleteUrl= actionEntity.getMethod().split("/");
+        switch (deleteUrl.length){
             case 2:
-                requestEntity=getAlterTable(actionEntity);
-                alterTable(requestEntity);
+                requestEntity=getDeleteDB(actionEntity);
+                deleteDB(requestEntity);
                 break;
             case 3:
-                requestEntity=getOpenTransaction(actionEntity);
-                openTransaction(requestEntity);
-                break;
-            case 4:
-                requestEntity=getCloseTransaction(actionEntity);
-                closeTransaction(requestEntity);
-                break;
-            case 5:
-                requestEntity=getMergeVersion(actionEntity);
-                mergeVersion(requestEntity);
-                break;
-            case 6:
-                requestEntity=getUseVersion(actionEntity);
-                useVersion(requestEntity);
-                break;
-            case 7:
-                requestEntity=getShowVersion(actionEntity);
-                showVersion(requestEntity);
-                break;
-            case 8:
-                requestEntity=getSingleSearch(actionEntity);
-                singleSearch(requestEntity);
-                break;
-            case 9:
-                requestEntity=getDeleteCells(actionEntity);
-                deleteCells(requestEntity);
-                break;
-            case 10:
-                requestEntity=getUpdateCells(actionEntity);
-                updateCells(requestEntity);
-                break;
-            case 11:
-                requestEntity=getMultiSearch(actionEntity);
-                multiSearch(requestEntity);
-                break;
-            case 12:
-                requestEntity=getDeleteVersion(actionEntity);
-                deleteVersion(requestEntity);
+                requestEntity=getDeleteTable(actionEntity);
+                deleteTable(requestEntity);
                 break;
             default:
-                System.out.println("未知错误-----------------");
+                System.out.println("the URL Segment is error"+"给出提示（把Delete开头的所有的命令返还给他");
         }
+    }
+
+    private static RequestEntity getDeleteTable(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getDeleteDB(ActionEntity actionEntity) {
+    }
+
+    private static void handlePost(ActionEntity actionEntity) {
+        String[] postUrl=actionEntity.getMethod().split("/");
+        switch (postUrl.length){
+            case 2:
+                if ("_open".equals(postUrl[1])){
+                    requestEntity=getOpenTransaction(actionEntity);
+                    openTransaction(requestEntity);
+                }else if ("_close".equals(postUrl[1])){
+                    requestEntity=getCloseTransaction(actionEntity);
+                    closeTransaction(requestEntity);
+                }else {
+                    System.out.println("the URL Segment is error"+"给出提示（把POST开头的所有的命令返还给他");
+                }
+                break;
+            case 3:
+                if ("_insert".equals(postUrl[2])){
+                    requestEntity=getPutCells();
+                    putCells(requestEntity);
+                }else if ("alter".equals(postUrl[2])){
+                    requestEntity=getAlterTable(actionEntity);
+                    alterTable(requestEntity);
+                }else if ("_merge".equals(postUrl[2])){
+                    requestEntity=getMergeVersion(actionEntity);
+                    mergeVersion(requestEntity);
+                }else if ("_use".equals(postUrl[2])){
+                    requestEntity=getUseVersion(actionEntity);
+                    useVersion(requestEntity);
+                }else if ("_showVersion".equals(postUrl[2])){
+                    requestEntity=getShowVersion(actionEntity);
+                    showVersion(requestEntity);
+                }else if ("_search".equals(postUrl[2])){
+                    requestEntity=getSingleSearch(actionEntity);
+                    singleSearch(requestEntity);
+                }else if ("_delete".equals(postUrl[2])){
+                    requestEntity=getDeleteCells(actionEntity);
+                    deleteCells(requestEntity);
+                }else if ("_update".equals(postUrl[2])){
+                    requestEntity=getUpdateCells(actionEntity);
+                    updateCells(requestEntity);
+                }else if ("_mget".equals(postUrl[2])){
+                    requestEntity=getMultiSearch(actionEntity);
+                    multiSearch(requestEntity);
+                } else {
+                    System.out.println("the URL Segment is error"+"给出提示（把POST开头的所有的命令返还给他");
+                }
+//                switch (postUrl[2]){
+//                    case "_insert":
+//                    case "alter":
+//                    case "_merge":
+//                    case "_use":
+//                    case "_showVersion":
+//                    case "_search":
+//                    case "_delete":
+//                    case "_update":
+//                    case "_mget":
+//                    default:
+//                        System.out.println("the URL Segment is error"+"给出提示（把POST开头的所有的命令返还给他");
+//                }
+            default:
+                System.out.println("the URL Segment is error"+"给出提示（把POST开头的所有的命令返还给他");
+
+        }
+    }
+
+    private static RequestEntity getMultiSearch(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getUpdateCells(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getDeleteCells(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getSingleSearch(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getShowVersion(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getUseVersion(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getMergeVersion(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getAlterTable(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getPutCells() {
+    }
+
+    private static RequestEntity getCloseTransaction(ActionEntity actionEntity) {
+    }
+
+    private static RequestEntity getOpenTransaction(ActionEntity actionEntity) {
+    }
+
+    private static boolean isPost(ActionEntity actionEntity) {
+        return "POST".equals(actionEntity.getMethod());
+    }
+
+    private static boolean isDelete(ActionEntity actionEntity) {
+        return "DELETE".equals(actionEntity.getMethod());
+    }
+
+    private static boolean isPut(ActionEntity actionEntity) {
+        return "PUT".equals(actionEntity.getMethod());
+    }
+
+    public static void main(String[] args) {
+        ActionEntity actionEntity=new ActionEntity();
+        String[] split = actionEntity.getMethod().split("/");
     }
 }
