a0b72a293ade921feb1f7adce42235612a87719a
lhh1171 <2807652103@qq.com>
2022-07-22 19:31:06 +0800

    nettyServer over

--- src/main/java/input/Entity/Post/ActionEntity.java
+++ src/main/java/input/Entity/Post/ActionEntity.java
@@ -17,6 +17,46 @@ public class ActionEntity {
         Method = method;
         Url = url;
     }
+    public boolean containKey(String key){
+        if (RegularAttribute!=null&&CompoundAttribute!=null){
+            return (RegularAttribute.containsKey(key)||CompoundAttribute.containsKey(key));
+        }else {
+            return false;
+        }
+    }
+    public void setMethod(String method) {
+        Method = method;
+    }
+
+    public void setUrl(String url) {
+        Url = url;
+    }
+    public void addRegularAttribute(String key,Object obj){
+        if (RegularAttribute==null){
+            HashMap<String,Object> map=new HashMap<String, Object>();
+            map.put(key,obj);
+            setRegularAttribute(map);
+        }else {
+            this.RegularAttribute.put(key,obj);
+        }
+
+    }
+    public void addCompoundAttribute(String key,List<HashMap<String, String>> list){
+        if (CompoundAttribute==null){
+            Map <String,List<HashMap<String, String>>> map=new HashMap<String, List<HashMap<String, String>>>();
+            map.put(key,list);
+            setCompoundAttribute(map);
+        }else {
+            this.RegularAttribute.put(key,list);
+        }
+    }
+    public void setRegularAttribute(HashMap<String, Object> regularAttribute) {
+        RegularAttribute = regularAttribute;
+    }
+
+    public void setCompoundAttribute(Map<String, List<HashMap<String, String>>> compoundAttribute) {
+        CompoundAttribute = compoundAttribute;
+    }
 
     public String getMethod() {
         return Method;
@@ -32,6 +72,16 @@ public class ActionEntity {
 
     public Map<String, List<HashMap<String, String>>> getCompoundAttribute() {
         return CompoundAttribute;
+    }
+
+    @Override
+    public String toString() {
+        return "ActionEntity{" +
+                "Method='" + Method + '\'' +
+                ", Url='" + Url + '\'' +
+                ", RegularAttribute=" + RegularAttribute +
+                ", CompoundAttribute=" + CompoundAttribute +
+                '}';
     }
 }
 
--- src/main/java/input/Server/JsonToRestFulEntityHandler.java
+++ src/main/java/input/Server/JsonToRestFulEntityHandler.java
@@ -10,46 +10,95 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Iterator;
+import java.util.*;
 
 
 public class JsonToRestFulEntityHandler extends ChannelInboundHandlerAdapter {
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        String ip=ctx.channel().remoteAddress().toString();
-        String[] a= ip.split(":");
-        System.out.println(a[0]+"-->"+"已连接");
-    }
-    @Override
-    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        String ip=ctx.channel().remoteAddress().toString();
-        String[] a= ip.split(":");
-        System.out.println(a[0]+"-->"+"已断开");
-    }
+//    @Override
+//    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//        String ip=ctx.channel().remoteAddress().toString();
+//        String[] a= ip.split(":");
+//        System.out.println(a[0]+"-->"+"已连接");
+//    }
+//    @Override
+//    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+//        String ip=ctx.channel().remoteAddress().toString();
+//        String[] a= ip.split(":");
+//        System.out.println(a[0]+"-->"+"已断开");
+//    }
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ActionEntity actionEntity= toRestfulEntity(msg);
+        System.out.println("server接收到\n"+actionEntity);
         ctx.fireChannelActive();
     }
     public ActionEntity toRestfulEntity(Object msg) throws JSONException {
         FullHttpRequest fullHttpRequest =(FullHttpRequest) msg;
+        ActionEntity actionEntity = new ActionEntity(fullHttpRequest.getMethod().name(),fullHttpRequest.getUri());
         String content = fullHttpRequest.content().toString(CharsetUtil.UTF_8);
         System.out.println(content);
-        return jsonToJavaOfRestfulEntity(content);
+        return jsonToJavaOfRestfulEntity(content,actionEntity);
     }
     //解析json转换为实体
-    public  ActionEntity jsonToJavaOfRestfulEntity(String content) throws JSONException {
-        ActionEntity actionEntity = new ActionEntity();
+    public  ActionEntity jsonToJavaOfRestfulEntity(String content,ActionEntity actionEntity) throws JSONException {
         JSONObject jsonObject = new JSONObject(content.trim());
-        Iterator<String> keys = jsonObject.keys();
+        Iterator keys = jsonObject.keys();
         while(keys.hasNext()) {
-            String key = keys.next();
-            if (jsonObject.get(key) instanceof JSONObject) {
-                // do something with jsonObject here
+            String key = (String) keys.next();
+            Object o = jsonObject.get(key);
+            if (o instanceof JSONArray) {
+                System.out.println(key+": jsonArray类型");
+                addCompoundAttribute(key,(JSONArray) o,actionEntity);
+            }else {
+                System.out.println(key+": kv类型");
+                addRegularAttribute(key,o,actionEntity);
             }
         }
-
         return actionEntity;
+    }
+
+    private void addRegularAttribute(String key,Object o, ActionEntity actionEntity) {
+        //看当前key是否存在
+        if (actionEntity.containKey(key)){
+            System.err.println("key重复");
+        }
+        actionEntity.addRegularAttribute(key,o);
+    }
+
+    private void addCompoundAttribute(String key,JSONArray array, ActionEntity actionEntity) throws JSONException {
+        //看当前key是否存在
+        if (actionEntity.containKey(key)){
+            System.err.println("key重复");
+        }
+        List<String> cfNames=new ArrayList<String>();
+        if ("cf_names".equalsIgnoreCase(key)){
+            for(int i=0;i<array.length();i++) {
+                try{
+                    String str = array.getString(i);
+                    cfNames.add(str);
+                }catch (JSONException ignored){
+                    System.err.println("cfNames错误");
+                }
+            }
+            actionEntity.addRegularAttribute(key,cfNames);
+        }else {
+            List<HashMap<String, String>> hashMapList=new ArrayList<HashMap<String, String>>();
+            for(int i=0;i<array.length();i++) {
+                try{
+                    HashMap<String, String> hashMap=new HashMap<String, String>();
+                    JSONObject jo=array.getJSONObject(i);
+                    Iterator iterator = jo.keys();
+                    while(iterator.hasNext()){
+                        key = (String) iterator.next();
+                        hashMap.put(key,jo.getString(key));
+                    }
+                    hashMapList.add(hashMap);
+                }catch (JSONException ignored){
+                    System.err.println("json类型错误");
+                }
+            }
+            actionEntity.addCompoundAttribute(key,hashMapList);
+        }
     }
 }
 
