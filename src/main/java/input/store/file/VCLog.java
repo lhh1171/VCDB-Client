8a807c3a48b2645d5f3b60b9fb0b6c35c94f5004
lhh1171 <2807652103@qq.com>
2022-07-29 22:51:23 +0800

    admin

--- src/main/java/input/store/file/VCLog.java
+++ src/main/java/input/store/file/VCLog.java
@@ -5,8 +5,7 @@ import input.store.wal.WalEdit;
 import java.util.HashMap;
 
 public class VCLog {
-//    HLog Sequece File的Value是HBase的KeyValue对象，
-//    即对应HFile中的KeyValue，包括：row、column family、qualifier、timestamp、value，以及“Key Type”（比如PUT或DELETE)。
-    //rowKey,walEdit
+    //即对应HFile中的KeyValue，包括：row、column family、qualifier、timestamp、value，以及“Key Type”（比如PUT或DELETE)。
+    //rowKey,walEdit一对一的关系，hashmap应该换成一个index dataSet
     HashMap<String,WalEdit> entry;
 }
