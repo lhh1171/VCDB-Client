8a7be930edd09c18e33f1bf638ad1f8988c8fc8b
lhh1171 <2807652103@qq.com>
2022-08-11 19:04:48 +0800

    byte

--- src/main/java/input/store/region/VCRegion.java
+++ src/main/java/input/store/region/VCRegion.java
@@ -10,14 +10,12 @@ package input.store.region;
 
 public class VCRegion {
     /*
-    * Trailer (offset of other member)
+    * Trailer (offset of other member)(根据offset,可以拿到整个region的大小)
     * RegionMeta (类）
-    * MetaIndex int(每一个RegionMeta的物理地址,按照Key的顺序排列)(大小固定)
-    * DataIndex int(每一个KV的物理地址)(大小固定，到达后需要分裂split)
-    *
+    * MetaIndex int(每一个RegionMeta的物理地址,按照Key的顺序排列)(每个单位的大小固定)
+    * DataIndex int(每一个KV的物理地址)
     * MetaSet
     * DataSet   (不分，一直往后累加，不用打乱排序）
     * */
     private byte[] data = null;
-
 }
