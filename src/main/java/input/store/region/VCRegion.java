5354954cbaeffb3a5174b5ecd82516102345b669
lhh1171 <2807652103@qq.com>
2022-08-14 11:37:34 +0800

    region

---
+++ src/main/java/input/store/region/VCRegion.java
@@ -0,0 +1,17 @@
+package input.store.region;
+
+import java.util.List;
+
+/**
+ * @ClassName VCRegion
+ * @Description TODO
+ * @Author lqc
+ * @Date 2022/8/14 上午11:35
+ * @Version 1.0
+ */
+
+public class VCRegion {
+    //regionID,VCRegionStores
+    byte[] regionID;
+    List<VCRegionStore> vcRegionStores;
+}
